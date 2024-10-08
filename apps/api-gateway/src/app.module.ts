b628e5de6906d0f9e1285181a561c227df07593e
BlackMonkcr <aaron.navarro@utec.edu.pe>
2024-10-08 03:46:43 -0500

    Configuracion inicial microservicios y coneccion con user-service

---
+++ apps/api-gateway/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: 'user-service',
+          port: 3001,
+        },
+      },
+      // Registra otros servicios aquí...
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
